072c5011926855aecd650c62a3c5330dc45b61f7
Ismael Rodriguez <=>
2020-04-13 21:06:54 +0100

    add set up to connect gatsby with sanity graphql

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -27,6 +27,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'hr16dxf5',
+        dataset: 'production',
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.MY_SANITY_TOKEN,
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
