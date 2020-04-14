872ac9f14ba3a5d19f6f8db3c808cb96d0c5cbbc
Ismael Rodriguez <=>
2020-04-14 08:17:04 +0100

    Add dependencies , run script and gitignore

---
+++ studio/schemas/objects/infoDate.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'infoDate',
+  title: 'Info for the date',
+  type: 'object',
+  fields: [{
+    "name": "confirmed",
+    "type": "number",
+    "title": "Confirmed"
+  }, {
+    "name": "recovered",
+    "type": "number",
+    "title": "Recovered"
+  }, {
+    "name": "deaths",
+    "type": "number",
+    "title": "Deaths"
+  }]
+}
