894f80eaa683d7d06dc8997c3f7506c27d170fd7
Ismael Rodriguez <=>
2020-04-26 22:49:15 +0100

    web(index): Add BasicTable

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, StaticQuery, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import CountryList from '../components/country-list'
+import BasicTable from '../components/tables/basic-table'
 
 const IndexPage = () => (
   <StaticQuery
@@ -31,12 +32,34 @@ const IndexPage = () => (
             _id
           }
         }
+        allSanityTimeSeries {
+          nodes {
+            lastDate {
+              recovered
+              deaths
+              confirmed
+            }
+            country {
+              _id
+              name
+            }
+          }
+        }
       }`
     }
     render ={ data => {
       const countriesAggregated = data.allSanityCountryInfoAggregated.nodes
+      const aggregatedCountriesRaw = data.allSanityTimeSeries.nodes
+      const aggregatedCountries = aggregatedCountriesRaw.reduce((acc, infoCountry) => {
+        return [...acc, {
+          ...infoCountry.country,
+          ...infoCountry.lastDate
+        }]
+      }, [])
+
       return (
         <Layout>
+          <BasicTable data={aggregatedCountries} />
           <CountryList list={countriesAggregated} />
           <Link to="/page-2/">Go to page 2</Link>
         </Layout>
